48b0923a0331adbff76afee888a18ae1c41d8b8d
amirhrb <amirrabi70@gmail.com>
2022-07-30 14:37:54 +0430

    debug

--- app.js
+++ app.js
@@ -1,71 +1,58 @@
+const addBtn = document.querySelector(".add-btn");
 
-const addBtn=document.querySelector(".add-btn");
+const mainDiv = document.querySelector(".main-div");
+const innerItem = document.querySelector(".item1");
+const inp = document.querySelector(".inputTodo");
+const removeAllBtn = document.querySelector(".removeAll");
+let removeBtnEl = document.querySelectorAll(".removebtn");
+let items = null;
+let itemCount = 1;
 
-const mainDiv=document.querySelector(".main-div");
-const innerItem=document.querySelector(".item1");
-const inp=document.querySelector(".inputtodo");
-const removeAllBtn=document.querySelector(".removeAll")
-let removeBtnEl=document.querySelectorAll(".removebtn");;
-let items=null;
-let itemCount=1;
+let itemDivs = [];
+let removeButtons = [];
+let checkButtons = [];
 
-let arrItems=[];
-let itemDivs=[];
-let removeButtons=[];
-let checkButtons=[];
-let clickonremoveevent=null;
-// item.appendChild()
-addBtn.addEventListener("click",addfunc)
+addBtn.addEventListener("click", addFunc);
 
-function addfunc(){
-    if(inp.value=="")return;
-    
-    let item =document.createElement("div");
-    item.classList.add("items");
-    
-    item.innerHTML=`<button class="removebtn item-btn">
+function addFunc() {
+  if (inp.value == "") return;
+
+  let item = document.createElement("div");
+  item.classList.add("item");
+
+  item.innerHTML = `<button class="removebtn item-btn">
     <i class="fas fa-minus-circle"></i></button>
     <span class="toDoText">${inp.value}</span>
     <button class="fa-checkBtn"><i class="fas fa-check">
     </i></button>
     `;
-    itemCount++;
-    inp.value="";
-    mainDiv.appendChild(item);
+  itemCount++;
+  inp.value = "";
+  mainDiv.appendChild(item);
 
-    items=document.querySelectorAll(".items");
-     itemDivs=[...items]
-     checkButtons.push(itemDivs[itemDivs.length-1].children[2])
-     removeButtons.push(itemDivs[itemDivs.length-1].children[0])
- }    
-    
+  items = document.querySelectorAll(".item");
+  itemDivs = [...items];
+  checkButtons.push(itemDivs[itemDivs.length - 1].children[2]);
+  removeButtons.push(itemDivs[itemDivs.length - 1].children[0]);
+  //
+  removeButtons.forEach((el) =>
+    el.addEventListener("click", (e) => {
+      e.target.parentElement.parentElement.remove();
+    })
+  );
+  checkButtons.forEach((el) => {
+    el.addEventListener("click", (e) => {
+      if (
+        e.target.parentElement.parentElement.style.textDecoration ===
+        "line-through"
+      ) {
+        e.target.parentElement.parentElement.style.textDecoration = "none";
+      } else {
+        e.target.parentElement.parentElement.style.textDecoration =
+          "line-through";
+      }
+    });
+  });
+}
 
-
-document.addEventListener("click",()=>{
-    removeButtons.forEach(e=>e.addEventListener("click",()=>{e.parentElement.remove();
-    }))
-    
-   checkButtons.forEach(e=>{
-    e.addEventListener("click",
-    ()=>{  
-         if( e.parentElement.style.textDecoration=="line-through"){
-   e.parentElement.style.textDecoration="none"}
-        else{e.parentElement.style.textDecoration="line-through"}
-     })})
-    }
-)
-
-
-
-removeAllBtn.addEventListener("click",()=>items.forEach(e=>e.remove()))
-
-    
-
-
-    // e.remove()  
-// }
-
-// const itemsClassList=
-// document.getElementsByClassName(".items");
-
-// itemsClassList[0].style.color="red"
+removeAllBtn.addEventListener("click", () => items.forEach((e) => e.remove()));
